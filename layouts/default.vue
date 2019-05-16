51d6732629312202c34db5eb8b9e33d0ee8567e5
FutureSaiyajin <futuresaiyajin@online.de>
2019-05-16 11:41:26 +0200

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="App">
+    <header></header>
+    <nav></nav>
+    <main>
+      <nuxt />
+    </main>
+    <aside></aside>
+    <footer></footer>
+  </div>
+</template>
+
+<style lang="scss">
+@import './holy-grail.scss';
+
+html {
+  background-color: theme('colors.white');
+  font-family: theme('fontFamily.sans');
+}
+</style>
